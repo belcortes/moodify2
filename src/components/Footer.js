0715b00a64323110f19fe4730462e99dee83c8f5
belcortes <belcortes@gmail.com>
2018-06-04 12:46:48 -0700

    added background color animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,23 @@ import Modal from 'react-modal';
 
 Modal.setAppElement('#root')
 
+const customStyles = {
+  content : {
+    top                   : '20%',
+    left                  : '20%',
+    right                 : '20%',
+    bottom                : '20%',
+    border                : 'none',
+    background            : '#2e94bb',
+    boxShadow             : '0 20px 40px 0 rgba(0,0,0,.33)',
+    color                 : 'white',
+    padding: '4em',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+};
+
 class Footer extends Component {
   state = {
     modalIsOpen: false
@@ -20,10 +37,10 @@ class Footer extends Component {
     return (
   		<div className='footer'>
         <button onClick={this.openModal} className='footer_button'>About</button>
-        <Modal isOpen={this.state.modalIsOpen}>
-          <button onClick={this.closeModal}>close</button>
-          <p>Does your music affect your mood?</p>
-          <p>In this app, music positiveness is found by calculating the <a href='#'>valence</a> of selected songs. Valence describes the musical positiveness conveyed by a track</p>
+        <Modal isOpen={this.state.modalIsOpen} style={customStyles}>
+          <button className='footer_button footer_button-close' onClick={this.closeModal}>Close</button>
+          <h2>Does your music affect your mood?</h2>
+          <p className='footer_about-body'>In this app, music positiveness is found by calculating the valence of selected songs. Valence describes the musical positiveness conveyed by a track</p>
         </Modal>
   		</div>
   	)
