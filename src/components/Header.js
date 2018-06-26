47204e321e525b3e006fa148d6b1f3061010cce8
belcortes <belcortes@gmail.com>
2018-06-26 16:32:52 -0700

    some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = (props) => {
 	return (
 		<div className='header'>
-      <h3>MOODIFY <span className='header-subtitle'>how happy	 is your music</span></h3>
+      <h3>MOODIFY <span className='header-subtitle'>HOW HAPPY IS YOUR MUSIC</span></h3>
 			{!props.user ? <p>Loading...</p> : <div className='header-options'>{props.user.display_name}<span className='header-subtitle'><a href='https://accounts.spotify.com/en/logout'>logout</a></span></div>}
 		</div>
 	)
