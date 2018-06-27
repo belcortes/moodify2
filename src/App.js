15729002e528b74127a0eae900505c4e212b1415
belcortes <belcortes@gmail.com>
2018-06-26 17:13:31 -0700

    adding transitions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import './App.css';
+import { CSSTransitionGroup } from 'react-transition-group'
 
+import './App.css';
 import { fetchPlaylists, fetchUser } from './actions/index'
 import PlaylistList from './containers/PlaylistList'
 import ActivePlaylist from './containers/ActivePlaylist'
@@ -16,14 +17,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header user={this.props.user} />
-        <div className='app-container'>
-          <PlaylistList playlists={this.props.playlists} />
-          <ActivePlaylist />
+      <CSSTransitionGroup
+        transitionName="app"
+        transitionAppear={true}
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
+        <div className="App">
+          <Header user={this.props.user} />
+          <div className='app-container'>
+            <PlaylistList playlists={this.props.playlists} />
+            <ActivePlaylist />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </CSSTransitionGroup>
+
+
     );
   }
 }
--- src/containers/ActivePlaylist.js
+++ src/containers/ActivePlaylist.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Track from '../components/Track'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 class ActivePlaylist extends Component {
 
@@ -9,16 +10,16 @@ class ActivePlaylist extends Component {
 			return <div className='app-container_outer-box app-container_outer-box_loading transform'><p className='app-container_header'>Select a playlist to get started</p></div>
 		}
 
+		const items = this.props.tracks.items.map((track, i) => {
+			return <Track key={i} track={track} />
+		});
+
 		return (
 			<div className='app-container_outer-box'>
 				<p className='app-container_header-with-subtitle'>{this.props.valence}% happy<br/><span className='app-container_header_subtitle'>{this.props.playlist.name} | {this.props.tracks.total} tracks</span></p>
 				<div className='app-container_inner-box'>
 					<ul>
-						{
-							this.props.tracks.items.map((track, i) => {
-								return <Track key={i} track={track} />
-							})
-						}
+						{items}
 					</ul>
 				</div>
 			</div>
